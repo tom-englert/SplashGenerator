3767316cdb247c092044e6f30af85150cfdf9b44
tom-englert <mail@tom-englert.de>
2016-07-18 20:01:54 +0200

    V 1.0.1

--- Release notes.md
+++ Release notes.md
@@ -1,2 +1,5 @@
-# 1.0.0.0
-* Initial version
+1.0.1.0
+* Load target with dependent assemblies.
+
+1.0.0.0
+* Initial version.
--- SplashGenerator/Properties/AssemblyInfo.cs
+++ SplashGenerator/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ namespace SplashGenerator.Properties
 {
     internal class Product
     {
-        public const string Version = "1.0.0.0";
+        public const string Version = "1.0.1.0";
     }
 }
