2570a558e04c9e6b43ced69f48579bd04bd7738f
tom-englert <mail@tom-englert.de>
2017-11-25 14:17:12 +0100

    Remove code dependency to Microsoft.Build

--- SplashGenerator/Program.cs
+++ SplashGenerator/Program.cs
@@ -4,8 +4,6 @@
     using System.IO;
     using System.Linq;
 
-    using Microsoft.Build.Evaluation;
-
     internal class Program
     {
         [STAThread]
@@ -13,25 +11,13 @@
         {
             try
             {
-                var projectFileName = args.FirstOrDefault();
+                var splashResourceName = args.FirstOrDefault();
                 var targetPath = args.Skip(1).FirstOrDefault();
 
-                if (string.IsNullOrEmpty(projectFileName) || string.IsNullOrEmpty(targetPath))
-                    throw new InvalidOperationException("Expected arguments: <project file> <target file name>");
+                if (string.IsNullOrEmpty(splashResourceName) || string.IsNullOrEmpty(targetPath))
+                    throw new InvalidOperationException("Expected arguments: <splash image name> <target file name>");
 
                 var targetDirectory = Path.GetDirectoryName(targetPath);
-
-                var projectCollection = ProjectCollection.GlobalProjectCollection;
-                var project = projectCollection?.LoadProject(projectFileName);
-                if (project == null)
-                    throw new InvalidOperationException($"'{projectFileName}' does not seem to be a valid project file.");
-
-                var splashResourceName = project.GetItems(@"SplashScreen")
-                    .Select(item => item.EvaluatedInclude)
-                    .FirstOrDefault();
-
-                if (string.IsNullOrEmpty(splashResourceName))
-                    throw new InvalidOperationException($"The project '{projectFileName}' does not contain a splash screen image. Add an image to the project and set it's build action to SplashScreen.");
 
                 using (var bitmapData = new MemoryStream())
                 {
